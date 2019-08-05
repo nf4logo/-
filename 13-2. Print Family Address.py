b8d3c25b0293b28bf7d87d1437a8dc52c3fde93f
nf4logo <44864797+nf4logo@users.noreply.github.com>
2019-08-05 14:31:38 +0800

    Create 13-2. Print Family Address.py

---
+++ 13-2. Print Family Address.py
@@ -0,0 +1,11 @@
+def printMyAddress(myName):
+    print(myName)
+    print("123 Main Street")
+    print("Ottawa, Ontario, Canada")
+    print("K2M 2E9")
+    print()
+
+printMyAddress("Carter Sande")
+printMyAddress("Warren Sande")
+printMyAddress("Kyra Sande")
+printMyAddress("Patricia Sande")
