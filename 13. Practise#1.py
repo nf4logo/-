9dff28a34b96f31d458cf749639c130122d0ef5d
nf4logo <44864797+nf4logo@users.noreply.github.com>
2019-08-05 14:34:26 +0800

    Create 13. Practise#1.py

---
+++ 13. Practise#1.py
@@ -0,0 +1,11 @@
+def printName():
+    print("TTTTTTT  EEEEEEE  RRRRR    EEEEEE   SSSSSSS         A")
+    print("   T     E        R    R   E       S               A A")
+    print("   T     E        R     R  E        S             A   A")
+    print("   T     EEEEEEE  R    R   EEEEEE    SSSSSS      A     A")
+    print("   T     E        RRRRR    E               S    AAAAAAAAA")
+    print("   T     E        R    R   E              S    A         A")
+    print("   T     EEEEEEE  R     R  EEEEEE   SSSSSS    A           A")
+    
+for i in range(5):
+    printName()
